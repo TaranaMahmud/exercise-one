4fe3baab15c1e94eaa0c95f0bc13dbed7641a3a1
TaranaMahmud <taranamahmud17@gmail.com>
2022-04-25 21:50:13 -0300

    Executed different functions

---
+++ app.py
@@ -0,0 +1,32 @@
+#This is to print the string "Hello World"
+print("Hello World")
+
+#This is to find the length
+course = "Python programming"
+print(len(course))
+print(course[0:3])
+
+#This is to craete a new line
+course = "Python \nProgramming"
+print (course)
+
+#This is to concetanate first and last name and print it
+first = "Tarana"
+last = "Mahmud"
+full = f"{first} {last}"
+print (full)
+
+#This is to print the name in all upper case
+name = "tarana mahmud"
+print(name.upper())
+
+#This is to capitalize the first letter of each word
+print(name.title())
+
+#This is to find the value in integer form
+print(10 // 3)
+
+#This is to find the value to 'y' when the value of 'x' is given.
+x = input("x: ")
+y = int(x) + 1
+print(f"x : {x}, y : {y}")
